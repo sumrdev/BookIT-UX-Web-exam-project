efd8b367983ecbe48d34ea236a58d6469718c00d
davidfeldner <dafe@itu.dk>
2023-05-16 16:44:26 +0200

    Setup swagger

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,9 +4,15 @@ import { rootHandler, helloHandler } from './handlers';
 const app = express();
 const port = process.env.PORT || '3000';
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.get('/', rootHandler);
 app.get('/hello/:name', helloHandler);
 
 app.listen(port, () => {
     console.log(`Listening to requests on http://localhost:${port}`);
 });
+//const swaggerAutogen = require('swagger-autogen')();
