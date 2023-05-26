8fbea5b3e060701a44d3c1e66729053ae9611620
davidfeldner <dafe@itu.dk>
2023-05-26 23:06:15 +0200

    Basic metadata and favicon

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app';
 import rootLayout from '../components/layout';
 import '../src/app/globals.css'
 import { Lexend } from 'next/font/google'
+import Head from 'next/head';
 
 const lexend = Lexend({ weight: ["300", "400", "500", "600", "700", "800", "900"], subsets: ['latin'] })
 
@@ -19,6 +20,17 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => rootLayout({ page }));
  
-  return <main className={lexend.className}>{getLayout(<Component {...pageProps} />)}</main>;
+  return (
+    <>
+      <Head>
+        <title>BookIT</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <main className={lexend.className}>
+        {getLayout(<Component {...pageProps} />)}
+      </main>
+    </>
+  );
 }
 export default App;
