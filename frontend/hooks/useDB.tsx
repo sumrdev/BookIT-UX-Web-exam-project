cbaaf69a5ac3afaa86d27e6d5353a6bbd3949948
davidfeldner <dafe@itu.dk>
2023-05-26 19:22:23 +0200

    Add useDB hook to fetch from database

---
+++ frontend/hooks/useDB.tsx
@@ -0,0 +1,44 @@
+import { use, useEffect, useState } from "react";
+
+function useDB(apiPath: string) {
+    const [data, setData] = useState<any>("")
+    const [loading, setLoading] = useState<boolean>(false)
+    const [error, setError] = useState<any>(null)
+
+
+    useEffect(() => {
+        (async function fetchData() {
+            try {
+                const token = document.cookie.split("=")[1];
+                if (!token) throw new Error("Unauthorized");
+                setLoading(true)
+                console.log("fetching data at: " + `${process.env.NEXT_PUBLIC_DB_ENDPOINT}/${apiPath}`)
+                const response = await fetch(`${process.env.NEXT_PUBLIC_DB_ENDPOINT}/${apiPath}`, {
+                    headers: {
+                        'Content-type': 'application/json',
+                        'Authorization': "Bearer " + token, // notice the Bearer before your token
+                    },
+                })
+                if(response.status === 401) {
+                    throw new Error("Unauthorized")
+                }
+                const data = await response.json()
+                setData(data);
+                setError(null) 
+            } catch (error) {
+                setError(error)
+            } finally {
+                setLoading(false)
+            }
+        })();
+    }, [apiPath])
+
+    useEffect(() => {
+        if (!loading && error) console.log(error)
+        if (!loading && error && error.message === "Unauthorized") window.location.href = "/login"
+    }, [error])
+    
+    return { data, loading, error };
+}
+
+export { useDB }
