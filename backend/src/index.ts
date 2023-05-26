2667141133c5923c743650750be3b32ada8308e0
davidfeldner <dafe@itu.dk>
2023-05-26 22:36:16 +0200

    Change backend port to 4000

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ require("dotenv").config(
 import express from 'express';
 
 const app = express();
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '4000';
 
 // const jsonwebtoken = require('jsonwebtoken');
 //const swaggerAutogen = require('swagger-autogen')();
